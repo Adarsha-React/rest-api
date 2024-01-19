be0c6aafa94c4083cc788d1029cb9747a75c5557
Adarsha-React <adarshareact2023@gmail.com>
2024-01-19 09:35:45 +0530

    Added GET_POST method: Tested via POSTMAN

--- README.md
+++ README.md
@@ -32,3 +32,40 @@
 
 - finally: npm run start
   Server is up: http://localhost:5050/
+
+- install npm install nodemon -D
+
+  - this will keep the sever in sync with any updates on editors, kind of hot module replacement
+  - updated package.json - "start": "nodemon index.js" previously: "start": "node index.js"
+
+- Created a mock folder and added mockData.js
+
+  - it contains few todos information in todos array and it is exported
+
+- imported the same in index.js
+
+  - remember the path should include the file extension as well.
+    - example: import { todos } from "./mocks/mockData.js" // named import
+
+- Route Configuaration
+
+  - Request URL(home page): http://localhost:5050/
+  - Request URL(todos): http://localhost:5050/todos
+
+  **_Express.js server_**
+
+  - Created a express.js server and started it.
+  - Added get and post methods.
+  - Initially, we have added dummy data (it should come from backend - code should be written for that)
+  - Tried to get all todos which are present in mockdata.js
+  - Tested it through **_Postman_** -
+    - Created a new collection - Todos - CRUD operations
+      - Added new request (Read_GET) - No need to send any body data
+        - Path/URL - http://localhost:5050/api/todos
+      - Added new request (Read_ID) - it accepts id to read particular record from the backend
+        - Path/URL - http://localhost:5050/api/todos/1
+      - Added create request (Create_POST)
+        - Need to the data which we need to add to todos in the body
+          - the same data can be retrieved from request.body in "POST" callback function
+          - use body-parser middleware to stringify the serialized data.
+          - added check if data is already present or not then insert the data to todos array.
--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { todos } from "./mocks/mockData.js";
 
 const app = express(); // getting the express instance which will have all the methods/functions
-
+app.use(bodyParser.json()); // added this middleware to pass all the body data as Stringfied serialized fashion
 const PORT = process.env.PORT || 5050; // port# which our backend server will run
 
 //Configuring routes - give some response for any request
 
 //for all root level - router
 app.all("/", (req, res) => {
-  console.log(req); // request(req) -> anything which comes from client to server
-  console.log(res); // response(res) -> anything which goes from server to clinet
+  // console.log(req); // request(req) -> anything which comes from client to server
+  // console.log(res); // response(res) -> anything which goes from server to clinet
 
   res.send("I'm up and running"); // simple message we are sending
+});
+
+//Read - method: GET
+/**
+ * In this place we can make a "DB connection" and get the data from the database.
+ * now we have just created the mockData from import it.
+ */
+
+app.get("/api/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.get("/api/todos/:id", (req, res) => {
+  const reqId = req.params.id;
+  const reqData = todos.filter((todo) => todo.id === reqId);
+  res.json(reqData);
+});
+
+app.post("/api/todos", (req, res) => {
+  const newTodo = req.body;
+  const index = todos.findIndex((todo) => todo.id === newTodo.id);
+  console.log(index);
+  if (index == -1) {
+    todos.push(newTodo);
+    //sending the response to client
+    res.json({
+      message: "new todo added successfully!!",
+    });
+  } else {
+    res.json({
+      message: "Record is already present!!",
+    });
+  }
 });
 
 // adding the listen method which should respond to client requests
